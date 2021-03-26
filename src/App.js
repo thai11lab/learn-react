4ee3db14e1873a8cde89f62f24033cddc170def4
thai11lab <anhthai2000atm@gmail.com>
2021-03-26 14:30:17 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import ColorBox from "./ColorBox";
+import React from "react";
 
 function App() {
+  const name="Đàm Anh Thái";
+  const age =20;
+  const family ={
+    human1:{
+      name : "Nguyễn Thị Ngoai",
+      age  : 40
+    },
+    human2:{
+      name : "Đàm Anh Sơn",
+      age : 12
+    }
+  }
+  const list =[
+    {
+      name:"Thái",
+    },
+    {
+      name:"Nhựt"
+    }
+  ]
+  const sex = true;
   return (
     <section>
-      <ColorBox color="red" ></ColorBox>
-      <ColorBox color="yellow" name="Huy"></ColorBox>
-      <ColorBox color="green" name="Hoàng"></ColorBox>
+      <h1>Hello {name}</h1>
+      <p>Giới tính : {sex ?<p style={{backgroundColor:"red"}}>Nam</p>:<p style={{backgroundColor:"green"}}>Nữ</p>}</p>
+      {sex &&(
+          <div>
+            <p>Đàm Anh Thái 1</p>
+            <p>Đàm Anh Thái 1</p>
+            <p>Đàm Anh Thái 1</p>
+          </div>
+        )
+      }
+      <p>Sử dụng reactframment</p>
+      {sex &&(
+          <React.Fragment>
+            <p>Thẻ 1</p>
+            <p>Thẻ 2</p>
+            <p>Thẻ 3</p>
+          </React.Fragment>
+        )
+      }
+
+      <p>Render object</p>
+      {list.map((value)=>(
+        <li>{value.name}</li>
+      ))}
     </section>
   );
 }
