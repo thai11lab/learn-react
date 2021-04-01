ad8e6d402728c8d4090c13e32fe744a7fed2c47e
thai11lab <anhthai2000atm@gmail.com>
2021-04-01 22:21:20 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import TodoFeature from "./features/Todo";
 import AlbumFeature from "./features/Album";
+import { Route,Link } from "react-router-dom";
 
 function App() {
   return (
     <section>
       <h1>Danh sách công việc</h1>
-      <TodoFeature></TodoFeature>
+      <div>
+        <Link to="/todos">To do</Link>
+        <Link to="/album">Album</Link>
+      </div>
+      <Route path="/todos" component={TodoFeature}></Route>
+      <Route path="/album" component={AlbumFeature}></Route>
     </section>
   );
 }
